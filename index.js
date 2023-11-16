debef29467b6bbbfbbb760004698c72fa55e4a04
Tibor <112723857+Tibor0106@users.noreply.github.com>
2023-11-16 16:10:35 +0000

    -

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
+const sqlite = require("sqlite3").verbose();
+const port = 3500
+let db = new sqlite.Database("db/main.db", (err) => {
+    if (err)
+        return console.log(err.message);
+    console.log("Connected to webserver database!");
+});
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
