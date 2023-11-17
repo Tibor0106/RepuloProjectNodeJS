121df8fcfa6bf43b6af2d5c2a7b0356658c387ce
Tibor <112723857+Tibor0106@users.noreply.github.com>
2023-11-17 17:41:46 +0000

    cors hozzáadva (react miatt kell, hogy medtudja hívni)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const sqlite = require("sqlite3").verbose();
+const cors = require('cors');
+app.use(cors());
 const port = 3500
 const adminkey = "admin";
 let destinations;
